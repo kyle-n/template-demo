fc5f3d4051bebd1e9c65d3e5ad049c3ecc5e7762
Kyle Nazario <knazario@bitovi.com>
2023-02-20 16:40:59 -0500

    Unit tests typing different passwords

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -16,16 +17,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'template-demo'`, () => {
+  it(`should have as title 'Template Demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('template-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('template-demo app is running!');
+    fixture.detectChanges()
+    const title = fixture.debugElement.query(By.css('h1')).nativeElement.textContent;
+    expect(title).toEqual('Template Demo');
   });
 });
--- src/app/signup-form/signup-form.component.spec.ts
+++ src/app/signup-form/signup-form.component.spec.ts
@@ -1,5 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignupFormComponent } from './signup-form.component';
+import { By } from '@angular/platform-browser';
+import { first, noop } from 'rxjs';
+import { Component, Input } from '@angular/core';
+import { PasswordStrength } from '../models/password-strength.type';
+
+@Component({
+  selector: 'app-password-strength-indicator',
+  template: ''
+})
+class PasswordStrengthIndicatorStubComponent {
+  @Input() passwordStrength: PasswordStrength | null = null;
+}
 
 describe('SignupFormComponent', () => {
   let component: SignupFormComponent;
@@ -7,7 +19,10 @@ describe('SignupFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupFormComponent ]
+      declarations: [
+        SignupFormComponent,
+        PasswordStrengthIndicatorStubComponent
+      ]
     })
     .compileComponents();
   });
@@ -18,7 +33,48 @@ describe('SignupFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  function typePassword(password: string): void {
+    const passwordInput = fixture.debugElement
+      .query(By.css('#password-input'))
+      .nativeElement as HTMLInputElement;
+
+    passwordInput.value = password
+    passwordInput.dispatchEvent(new Event('input'))
+  }
+
+  it('should rate empty passwords as 0 strength', done => {
+    component.passwordStrength.pipe(first()).subscribe(
+      passwordStrength => expect(passwordStrength).toBe(0),
+      noop,
+      done
+    )
+    typePassword('')
   });
+
+  it('should require at least one number', done => {
+    component.passwordStrength.pipe(first()).subscribe(
+      passwordStrength => expect(passwordStrength).toBe(2),
+      noop,
+      done
+    )
+    typePassword('ABCDEF')
+  })
+
+  it('should require at least one capital letter', done => {
+    component.passwordStrength.pipe(first()).subscribe(
+      passwordStrength => expect(passwordStrength).toBe(2),
+      noop,
+      done
+    )
+    typePassword('123456')
+  })
+
+  it('should require at least one six characters', done => {
+    component.passwordStrength.pipe(first()).subscribe(
+      passwordStrength => expect(passwordStrength).toBe(2),
+      noop,
+      done
+    )
+    typePassword('1234A')
+  })
 });
